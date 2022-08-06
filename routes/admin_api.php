5f3dd40db4196be4ca4349ede7af1fdb6852fab5
Soneye Sina <advancoplanet@gmail.com>
2022-08-06 16:31:51 +0100

    rout update

--- routes/admin_api.php
+++ routes/admin_api.php
@@ -3,7 +3,7 @@
 use App\Http\Controllers\Administrator;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers;
-
+use App\Http\Enum\GroupEnum;
 
 Route::post('login', [Administrator\AdminAuthController::class, 'login']);
 Route::post('register', [Administrator\AdminAuthController::class, 'register']);
@@ -15,8 +15,6 @@ Route::middleware(['auth:sanctum', 'isTopLevelAdmin'])->group(function () {
     Route::get('list_administrators/{withDeleted?}', [Administrator\AccountsManagerController::class, 'list_admintrators']);
     Route::get('get_profile/{administrator}', [Administrator\AccountsManagerController::class, 'get_profile']);
     Route::get('get_admin_with_relations/{administrator}', [Administrator\AccountsManagerController::class, 'get_admin_with_relations']);
-
-    Route::get('get-cost/{service}/{attire}', [Administrator\ServiceCostController::class, 'get_service_cost']);
 
     Route::resource('attires',  Administrator\AttireTypeController::class);
     Route::resource('services',  Administrator\ServiceController::class);
@@ -35,6 +33,18 @@ Route::middleware(['auth:sanctum', 'isTopLevelAdmin'])->group(function () {
     Route::get('all-service-methods', [Administrator\ServiceMethodController::class, 'index']);
     Route::get('deleted-service-methods', [Administrator\ServiceMethodController::class, 'deleted']);
 
+    //Service Cost
+    Route::post('service-cost', [Administrator\ServiceCostController::class, 'store']);
+    Route::delete('service-cost/{serviceCost}', [Administrator\ServiceCostController::class, 'delete']);
+    Route::get('service-cost/{service}/{attire}', [Administrator\ServiceCostController::class, 'get_service_cost']);
+
+    //group
+    Route::get('groups' , function(){
+        return response()->json(
+            GroupEnum::cases(),
+            200
+        );
+    });
     
 
 });
