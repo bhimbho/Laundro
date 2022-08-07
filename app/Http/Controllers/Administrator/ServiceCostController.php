3f033cdb3cbcb880d2b4d49dc4dac9a147ae0294
Soneye Sina <advancoplanet@gmail.com>
2022-08-08 00:56:56 +0100

    Service Cost Update

--- app/Http/Controllers/Administrator/ServiceCostController.php
+++ app/Http/Controllers/Administrator/ServiceCostController.php
@@ -20,7 +20,7 @@ class ServiceCostController extends Controller
     }
 
     public function index(Service $service) {
-        return ServiceCost::with('service')->where('service_id', $service->id)->latest()->paginate(10);
+        return ServiceCost::with('service', 'attire_types')->where('service_id', $service->id)->latest()->paginate(10);
     }
 
     public function get_service_cost($service, $attire) {
