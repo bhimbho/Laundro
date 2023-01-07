bb8f4d5450483a2aed00e8f84247cab008856095
Soneye Sina <advancoplanet@gmail.com>
2023-01-07 17:00:39 +0100

    extension Validation

--- tests/Feature/AttireTypeTest.php
+++ tests/Feature/AttireTypeTest.php
@@ -33,4 +33,20 @@ class AttireTypeTest extends TestCase
 
         $response->assertSuccessful();
     }
+
+    public function test_correct_image_extension_is_validated()
+    {
+        Sanctum::actingAs(
+            Administrator::factory()->create()
+        );
+        Storage::fake();
+        $response = $this->postJson(route('attires.store'), [
+            'title' => 'Women Men2',
+            'attire_image' => UploadedFile::fake()->create('avatar.pdf'),
+            'group' => 'xxl'
+        ]);
+
+        $response->assertJsonStructure(['errors']);
+        $response->assertSeeText('The attire image must be an image');
+    }
 }
